cb7d8e5c8882c255a3a035f33d475b99ce57c305
GregRgy <gregrgy12@outlook.com>
2023-10-13 13:28:22 +0200

    Ajout des libellés de la page A propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,10 +8,10 @@ function About() {
     <>
         <ImageBanner/>
         <div className='about__container'>
-        <DescriptionPanel content="C'est très important" title="Fiabilité" />
-        <DescriptionPanel content="C'est très important" title="Respect" />
-        <DescriptionPanel content="C'est très important" title="Service" />
-        <DescriptionPanel content="C'est très important" title="Responsabilité" />
+        <DescriptionPanel content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." title="Fiabilité" />
+        <DescriptionPanel content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." title="Respect" />
+        <DescriptionPanel content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." title="Service" />
+        <DescriptionPanel content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." title="Sécurité" />
         </div>
     </>
   )
